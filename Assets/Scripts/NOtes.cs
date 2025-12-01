b2e73a85479ad9c16b3972e85068d29cab25be80
EReyes13 <itsmeelio13@gmail.com>
2025-12-01 18:17:27 -0500

    0.0.4
    Figured out Tile maps
    Create new level

---
+++ Assets/Scripts/NOtes.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class NOtes
+{
+    
+}
+
+/*For making tilemap levels, start with a new scene and have the tile package added( if selected 2d option, should be incuded already). Then, go to create, 2D object, Tilemap, and then whatever choice you desire.
+after that is done, it will create a grid and a tilemap child, add the tilemap, tilemap renderer, and tilemap collider 2d. Those will render and be the hitboxes for the tiles. 
+Once that is done, go to window, and find Tile Palette, and open it. There will be a tile palette for this project, but for future projects, add all the tile sprites that will be used in that tile palette window
+Once that is done, you can use the tools to add the tiles from the palette into the level itself.*/
